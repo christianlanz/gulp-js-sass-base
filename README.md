f5e315358967dbdfe098831ae3d2d622c548fefd
Christian Lanz <christian.l4nz@gmail.com>
2016-12-20 16:20:05 +0100

    Add base files and structure

---
+++ README.md
@@ -0,0 +1,45 @@
+# bootstrap-sass-base
+
+### Download required dependencies
+
+Install Homebrew:
+
+```sh
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+Install Node.js:
+
+```sh
+$ brew install node
+```
+
+Install Gulp.js:
+
+```sh
+npm install -g gulp-cli
+```
+
+Install Bower:
+
+```sh
+$ npm install -g bower
+```
+
+### Initialize project
+
+Fetch Node and Bower packages:
+
+```sh
+$ cd bootstrap-sass-base
+$ npm install
+$ gulp bower
+```
+
+### Usage
+
+Run the default task:
+
+```sh
+$ gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var htmlmin = require('gulp-htmlmin');
+var cleancss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
+var del = require('del');
+
+
+/* ----- Generic ----- */
+
+gulp.task('clean', function() {
+  del('./assets/css');
+  del('./out');
+});
+
+
+/* ----- Development ----- */
+
+gulp.task('sass', function() {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function() {
+  return gulp
+    .watch('./assets/scss/**/*.scss', ['sass'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', transpiling ...');
+    });
+});
+
+gulp.task('default', ['sass', 'sass:watch']);
+
+
+/* ----- Production ----- */
+
+gulp.task('html', function() {
+  return gulp.src('./*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./out'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('./assets/css/*')
+    .pipe(cleancss({
+      keepSpecialComments: 0
+    }))
+    .pipe(gulp.dest('./out/assets/css'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('./assets/fonts/*')
+    .pipe(gulp.dest('./out/assets/fonts'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('./assets/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./out/assets/js'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('./assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./out/assets/images'));
+});
+
+gulp.task('favicons', function() {
+  return gulp.src('./assets/favicons/*')
+    .pipe(gulp.dest('./out/assets/favicons'));
+});
+
+gulp.task('production', ['html', 'fonts', 'js', 'images', 'favicons'], function() {
+  // TODO: Replaced with gulp.series when updating to gulp v4.0
+  // Run sass and css task after one another
+  runSequence('sass', 'css');
+});
